35d9d5b5cceeb01222eee50795efd811ce5f5a70
hakozaru <t.morii@everyleaf.com>
2021-03-14 09:02:08 +0900

    演習6-8終了

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
 import AnocdoteForm from './components/AnocdoteForm'
-import { incrementVote } from './reducers/anecdoteReducer'
+import AnocdoteList from './components/AnocdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => {
-    return state.sort((anocA, anocB) => {
-      if(anocA.votes > anocB.votes) return -1
-      if(anocA.votes < anocB.votes) return 1
-      return 0
-    })
-  })
-  const dispatch = useDispatch()
-
-  const vote = (id) => dispatch(incrementVote(id))
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnocdoteList />
       <AnocdoteForm />
     </div>
   )
