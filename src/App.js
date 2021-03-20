5b7e7a0b03e95f84fddaf3745d860f8c8efee7a6
hakozaru <t.morii@everyleaf.com>
2021-03-21 08:52:55 +0900

    演習6-13終了

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnocdoteForm from './components/AnocdoteForm'
 import AnocdoteList from './components/AnocdoteList'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { initAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => {
+      dispatch(initAnecdotes(anecdotes))
+    })
+  }, [dispatch])
+
   return (
     <div>
       <Notification />
